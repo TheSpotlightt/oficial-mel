0c9a01e9b893dd30a3e92df2180459a8527b3c65
TheSpotlightt <samuelgobbo93@gmail.com>
2021-04-14 11:53:12 -0300

    first commit

---
+++ public/scripts/main.js
@@ -0,0 +1,8 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+	new Splide( '#image-slider', {
+        type: 'loop',
+        cover: true,
+        heightRatio: 0.5,
+        autoplay    : true,
+	} ).mount();
+} );
