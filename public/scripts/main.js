4df0318920d72e03a27a49e54ca1ce84e441187d
TheSpotlightt <samuelgobbo93@gmail.com>
2021-04-15 16:15:55 -0300

    feat: add new features and styles

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,8 +1,10 @@
 document.addEventListener( 'DOMContentLoaded', function () {
 	new Splide( '#image-slider', {
-        type: 'loop',
-        cover: true,
-        heightRatio: 0.5,
-        autoplay    : true,
+            type: 'loop',
+            cover: true,
+            heightRatio: 0.5,
+            autoplay    : true,
+            pagination: false,
+
 	} ).mount();
 } );
